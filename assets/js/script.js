87153cccd592291929b4a93d0c0bd4877ca74fc8
Priscilla <edpris.montalvo@gmail.com>
2022-08-10 00:58:29 -0500

    grade answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,62 @@
-var createQuestion = function(questionId) {
 
+var questionNumber = 0;
+var correctAnswers = 0;
+
+const removeChildren = (parent) => {
+    console.log(parent);
+    while (parent.lastChild) {
+        if(parent.lastChild.checked && questions[questionNumber - 1].correctAnswerIndex == parent.lastChild.id){
+            correctAnswers++;
+        }
+        parent.removeChild(parent.lastChild);
+    }
 };
 
-function startQuiz() {
+function startQuiz() { 
     console.log("startQuiz");
     var timerDisplay = document.querySelector('#timerDisplay');
     startTimer(30, timerDisplay);
+    displayQuestion();
     
-    //do more stuff
 };
+
+function displayQuestion() {
+    if(questions.length > questionNumber){
+        var answers = questions[questionNumber++].answers;
+        for(let i = 0; i < answers.length; i++){
+            var radiobox = document.createElement('input');
+            radiobox.type = 'radio';
+            radiobox.id = i;
+            radiobox.value = answers[i];
+            radiobox.name = "answer-choice"
+        
+            var label = document.createElement('label')
+            label.htmlFor = i;
+        
+            var description = document.createTextNode(answers[i]);
+            label.appendChild(description);
+        
+            var newline = document.createElement('br');
+        
+            var container = document.getElementById('question');
+            container.appendChild(radiobox);
+            container.appendChild(label);
+            container.appendChild(newline);
+        }
+    }
+    else{
+        //done with quiz
+        //display results
+        console.log(correctAnswers/questions.length * 100);
+    }
+}
+
+function getNextQuestion(){
+    var parent = document.getElementById("question");
+    
+    removeChildren(parent);
+    displayQuestion();
+}
 
 var startTimer = function(duration, display) {
     var timer = duration, seconds;
@@ -26,5 +74,29 @@ var startTimer = function(duration, display) {
         }
     }, 1000);
 };
+
+var questions = [
+    {
+        question: "question here?",
+        answers: [
+            "A", 
+            "B", 
+            "C"
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "question here?",
+        answers: [
+            "D", 
+            "E", 
+            "F"
+        ],
+        correctAnswerIndex: 0
+    }
+];
+
+e = document.getElementById("submit-ans");
+e.addEventListener("click", function(){getNextQuestion();}, false);
 
 
